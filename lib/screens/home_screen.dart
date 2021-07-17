8028bd8bbfbb101240d5ba302f6f8a28ffb2b415
Ritikag2906 <ritikag2906@gmail.com>
2021-07-17 23:09:43 +0530

    .

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,15 @@
+import 'dart:html';
+
 import 'package:catalog/models/catalog.dart';
+import 'package:catalog/widgets/themes.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:catalog/widgets/drawer.dart';
 import 'package:catalog/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
+// import 'package:flutter_catalog/widgets/themes.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -35,48 +40,59 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-      body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16,
-                  ),
-                  itemBuilder: (context, index) {
-                    final item = CatalogModel.items[index];
-                    return Card(
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: GridTile(
-                            child: Image.network(item.image),
-                            header: Container(
-                                child: Text(
-                                  item.name,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                padding: const EdgeInsets.all(12),
-                                decoration: BoxDecoration(
-                                  color: Colors.deepPurple,
-                                )),
-                            footer: Container(
-                                child: Text(
-                                  item.price.toString(),
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                padding: const EdgeInsets.all(12),
-                                decoration: BoxDecoration(
-                                  color: Colors.black,
-                                ))));
-                  },
-                  itemCount: CatalogModel.items.length,
-                )
-              : Center(
+        body: SafeArea(
+      child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatalogHeader(),
+              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                CatalogList()
+              else
+                Center(
                   child: CircularProgressIndicator(),
-                )),
-      drawer: MyDrawer(),
+                )
+            ],
+          )),
+    ));
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        "Catalog App".text.xl5.bold.color(Mytheme.darkBluishColor).make(),
+        "Trending Products".text.xl2.make()
+      ],
     );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({Key key, @required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox().white.square(100).make();
   }
 }
